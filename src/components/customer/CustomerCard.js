6c444ef918cd491850faf69f8fbefd9078f77fd0
Thomas Witherow <twitherow@bellsouth.net>
2021-07-08 15:58:17 -0500

    added files will finish later

--- src/components/customer/CustomerCard.js
+++ src/components/customer/CustomerCard.js
@@ -3,8 +3,10 @@
     import "./Customer.css"
     
     export const CustomerCard = () => (
+        <div className="customers">
         <section className="customer">
             <h3 className="customer__name">Hanna Hall</h3>
             <div className="customer__address">Address: 100 Infinity Way</div>
         </section>
+            </div>
     )
